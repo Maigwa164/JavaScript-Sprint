da71926beb8c579090bc3fbddd482f09f2b14a9f
Maigwa164 <davidmaigwa164@gmail.com>
2024-04-22 11:54:36 +0300

    Values and variable, Data types and Naming conventions

---
+++ script.js
@@ -0,0 +1,44 @@
+let js = 'amaizing';
+//if(js=='amaizing') alert("Hello Maddy Welcome to js");
+//console.log(20+20);
+/*
+console.log(40 + 30 - 9);
+console.log(23);
+
+//Naming convention
+let firtName = "Maddy";
+console.log(firtName);
+console.log(firtName);
+console.log(firtName);
+
+let firstJob = ("Freelancer");
+let currentJob = ("Sofware Engineer");
+
+console.log(firstJob);
+console.log(currentJob);
+*/
+
+//Variable and Data Types
+
+let maddy = true;
+console.log(maddy);
+console.log(typeof maddy);
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "Maddy");
+console.log(typeof Null);
+
+//Dynamic Typing 
+
+maddy = 'Mad man';
+console.log(typeof maddy);
+
+//undefined
+ let year;
+ console.log(year);
+
+ year = 2001;
+ console.log(year);
+ console.log(typeof year);
+
+ console.log(typeof null);
