4014d55d6d87a26395c504455f2867ef171f31e8
Maigwa164 <davidmaigwa164@gmail.com>
2024-06-04 16:55:32 +0300

    using strict mode

--- Section Two/script.js
+++ Section Two/script.js
@@ -1,1 +1,7 @@
-'use strict';
+// 'use strict';
+
+let hasDriversLicense = false;
+const passedTest = true;
+
+if(passedTest) hasDriversLicense = false;
+if(hasDriversLicense) console.log("can drive");
