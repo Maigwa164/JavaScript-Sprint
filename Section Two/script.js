43c737af457f002790f896996aa6fc32d10c27e4
Maigwa164 <davidmaigwa164@gmail.com>
2024-06-11 01:05:39 +0300

    Introduction to functions

--- Section Two/script.js
+++ Section Two/script.js
@@ -1,7 +1,32 @@
-// 'use strict';
+/*'use strict';
 
 let hasDriversLicense = false;
 const passedTest = true;
 
-if(passedTest) hasDriversLicense = false;
+if(passedTest) hasDriversLicense = true;
 if(hasDriversLicense) console.log("can drive");
+*/
+'use strict';
+
+function logger(){
+    console.log('My first function');
+}
+
+logger();
+logger();
+logger();
+
+function fruitProccesor(apple, orange){
+// console.log(apple,orange);
+const juice = `Juice with ${apple} apple and ${orange} oranges.`
+return juice;
+}
+
+// fruitProccesor(4,2);
+const appleJuice = fruitProccesor(4,0);
+console.log(appleJuice);
+
+// console.log(fruitProccesor(4,2));
+
+const appleOrangeJuice = fruitProccesor(3,5);
+console.log(appleOrangeJuice);
