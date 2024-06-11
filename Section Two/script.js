0f36c6bb9cccf740ba4e257d3772b705aff7f296
Maigwa164 <davidmaigwa164@gmail.com>
2024-06-11 10:09:19 +0300

    Function declaration and function expression

--- Section Two/script.js
+++ Section Two/script.js
@@ -1,12 +1,14 @@
-/*'use strict';
-
+'use strict';
+/*
 let hasDriversLicense = false;
 const passedTest = true;
 
 if(passedTest) hasDriversLicense = true;
 if(hasDriversLicense) console.log("can drive");
 */
-'use strict';
+
+/*
+//INTRODUCTION TO FUNCTIONS
 
 function logger(){
     console.log('My first function');
@@ -30,3 +32,32 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProccesor(3,5);
 console.log(appleOrangeJuice);
+*/
+
+
+
+//my logic
+
+function age(currentYear,birthYear){
+    const youraAge = currentYear - birthYear;
+    return youraAge;
+}
+
+const currentAge = age(2024,2001);
+console.log(currentAge);
+
+
+//Function Declaration
+function age1Calc(yearofBirth){
+return 2024 -yearofBirth;
+}
+const Age1 = age1Calc(2001);
+// console.log(Age1);
+
+//function expression
+const Age2Calc = function(yearofBirth){
+    return 2024 - yearofBirth;
+}
+const Age2 = Age2Calc(2000)
+
+console.log(Age1,Age2);
