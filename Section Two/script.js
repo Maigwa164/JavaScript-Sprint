5887eb7701de5780e14650eb19a28c4c2ae61b69
Maigwa164 <davidmaigwa164@gmail.com>
2024-07-30 13:11:24 +0300

    Coding challenge

--- Section Two/script.js
+++ Section Two/script.js
@@ -630,12 +630,13 @@ for(let i = 1; i<4;i++){
 }
     */
 
+/*
 //-----While Loop-----
-// let rep = 1;
-// while(rep<=10){
-//     console.log(`This is my ${rep} lift🏋🏾‍♀️`);
-//     rep++;
-// }
+let rep = 1;
+while(rep<=10){
+    console.log(`This is my ${rep} lift🏋🏾‍♀️`);
+    rep++;
+}
 
 let dice = Math.trunc((Math.random()*6)+1);
 // console.log(dice);
@@ -647,3 +648,64 @@ while(dice!==6){
         console.log(`Loop ended you picked a ${dice}`);
     }
 }
+*/
+
+/*
+Coding Challenge #4 
+Let's improve Steven's tip calculator even more, this time using loops! 
+Your tasks: 
+1. Create an array 'bills' containing all 10 test bill values 
+2. Create empty arrays for the tips and the totals ('tips' and 'totals') 
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate 
+tips and total values (bill + tip) for every bill value in the bills array. Use a for 
+loop to perform the 10 calculations! 
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52 
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the 
+tips and totals arrays 
+�
+� 
+Bonus: 
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as 
+an argument. This function calculates the average of all numbers in the given 
+array. This is a difficult challenge (we haven't done this before)! Here is how to 
+solve it: 
+4.1. First, you will need to add up all values in the array. To do the addition, 
+start by creating a variable 'sum' that starts at 0. Then loop over the 
+array using a for loop. In each iteration, add the current value to the 
+'sum' variable. This way, by the end of the loop, you have all values 
+added together 
+4.2. To calculate the average, divide the sum you calculated before by the 
+length of the array (because that's the number of elements) 
+4.3. Call the function with the 'totals' array */
+
+const billValue = [ 22, 295, 176, 440, 37, 105, 10, 1100, 86, 52 ];
+const tip = [];
+const totals = [];
+const calcTip = function(billValue){
+    if(billValue<=50>=300){
+         return 0.15*billValue;
+    }
+    else{
+         0.2*billValue;
+         return (0.2*billValue);
+    }
+}
+
+// const calcTotal = function(billValue){
+//     return calcTip(billValue) + billValue;
+// }
+
+// console.log(calcTip(billValue[3]))
+
+for(let i=0; i<10;i++){
+    // console.log(`The bill is: ${billValue[i]} The tip is: ${calcTip(billValue[i])}`);
+
+   
+    // console.log(`The bill is: ${billValue[i]} The tip is ${calcTip(billValue[i])}`);
+
+    tip.push(calcTip(billValue[i]));
+    totals.push(billValue[i]+calcTip(billValue[i]));
+}
+console.log(`This is the Bill: ${billValue}`)
+console.log(`The tip is: ${tip }`);
+console.log(`The total is: ${totals }`);
