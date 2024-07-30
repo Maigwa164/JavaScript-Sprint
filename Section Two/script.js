5b0d33e7632fc2374b5304ca3c707044f07cfa8c
Maigwa164 <davidmaigwa164@gmail.com>
2024-07-30 15:10:39 +0300

    Task two coding challenge

--- Section Two/script.js
+++ Section Two/script.js
@@ -678,16 +678,17 @@ added together
 length of the array (because that's the number of elements) 
 4.3. Call the function with the 'totals' array */
 
+
+// -------------------coding challenge one----------
 const billValue = [ 22, 295, 176, 440, 37, 105, 10, 1100, 86, 52 ];
-const tip = [];
+const tips = [];
 const totals = [];
 const calcTip = function(billValue){
-    if(billValue<=50>=300){
+    if(billValue>=50 && billValue<=300){
          return 0.15*billValue;
     }
     else{
-         0.2*billValue;
-         return (0.2*billValue);
+         return 0.2*billValue;
     }
 }
 
@@ -697,15 +698,48 @@ const calcTip = function(billValue){
 
 // console.log(calcTip(billValue[3]))
 
-for(let i=0; i<10;i++){
+for(let i=0; i<billValue.length;i++){
     // console.log(`The bill is: ${billValue[i]} The tip is: ${calcTip(billValue[i])}`);
 
    
     // console.log(`The bill is: ${billValue[i]} The tip is ${calcTip(billValue[i])}`);
-
-    tip.push(calcTip(billValue[i]));
-    totals.push(billValue[i]+calcTip(billValue[i]));
+    const tip = calcTip(billValue[i])
+    tips.push(tip);
+    totals.push(billValue[i]+ tip);
 }
 console.log(`This is the Bill: ${billValue}`)
-console.log(`The tip is: ${tip }`);
+console.log(`The tip is: ${tips }`);
 console.log(`The total is: ${totals }`);
+
+
+//-------coding challenge two------
+/*
+Bonus: 
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as 
+an argument. This function calculates the average of all numbers in the given 
+array. This is a difficult challenge (we haven't done this before)! Here is how to 
+solve it: 
+4.1. First, you will need to add up all values in the array. To do the addition, 
+start by creating a variable 'sum' that starts at 0. Then loop over the 
+array using a for loop. In each iteration, add the current value to the 
+'sum' variable. This way, by the end of the loop, you have all values 
+added together 
+4.2. To calculate the average, divide the sum you calculated before by the 
+length of the array (because that's the number of elements) 
+4.3. Call the function with the 'totals' array */
+
+
+const arr = [1,2,3,4,5,6,7,8,9,10]
+const calcAverage = function(arr){
+let sum = 0;
+for(let i = 0;i<arr.length;i++){
+    sum += arr[i];
+}
+return sum / arr.length;
+}
+
+console.log(calcAverage(arr));
+console.log(calcAverage(billValue));
+console.log(calcAverage(tips));
+console.log(calcAverage(totals));
+
