ab26f476e7efc953fb3a707d91e9f784899e3f65
Maigwa164 <davidmaigwa164@gmail.com>
2024-08-06 23:55:20 +0300

    Working with eventListener

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -14,3 +14,12 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 16;
 console.log(document.querySelector('.guess').value);
 */
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //   document.querySelector('.message').textContent = '🎉correct guess';
+  console.log(guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Number is empty🥲';
+  }
+});
