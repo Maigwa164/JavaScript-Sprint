05ce688e0c4b9c70d628e8eded26f9dc3dcf54c8
Maigwa164 <davidmaigwa164@gmail.com>
2024-08-06 22:49:56 +0300

    Selecting and manupulating elements using document.querySelector()

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,2 +1,16 @@
 'use strict';
+/*
 console.log(document.querySelector('.message').textContent);
+
+console.log(
+  (document.querySelector('.message').textContent = '🥳Correct Number')
+);
+// console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 16;
+console.log(document.querySelector('.guess').value);
+*/
