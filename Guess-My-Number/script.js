1833d7f2915759d72258d6b3639a4a883ba1619a
Maigwa164 <davidmaigwa164@gmail.com>
2024-08-07 02:00:48 +0300

    Handling on click event on the game

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -15,11 +15,33 @@ document.querySelector('.guess').value = 16;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   //   document.querySelector('.message').textContent = '🎉correct guess';
   console.log(guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'Number is empty🥲';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number🎊';
+  } else if (guess > secretNumber) {
+    score -= 1;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Too high 📈';
+    if (score <= 0) {
+      document.querySelector('.message').textContent = 'Game Over😔';
+      document.querySelector('.score').textContent = '0';
+    }
+  } else if (guess < secretNumber) {
+    score -= 1;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Too low 📉';
+    if (score <= 0) {
+      document.querySelector('.message').textContent = 'Game Over😔';
+      document.querySelector('.score').textContent = '0';
+    }
   }
 });
