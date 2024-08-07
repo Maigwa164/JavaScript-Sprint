e084b0f0ea8640a49e155f68c958b69a39274a2f
Maigwa164 <davidmaigwa164@gmail.com>
2024-08-07 03:57:19 +0300

    Reseting game default

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -15,18 +15,19 @@ document.querySelector('.guess').value = 16;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.number').textContent = secretNumber;
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   //   document.querySelector('.message').textContent = '🎉correct guess';
-  console.log(guess);
+  //   console.log(guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'Number is empty🥲';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number🎊';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
@@ -46,4 +47,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = '0';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = 'black';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start Guessing🤸🏾';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.guess').value = '';
 });
