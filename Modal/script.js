352513290212c04efbae14166a14450fd0a3549b
David Maigwa <davidmaigwa164@gmail.com>
2025-01-28 15:45:18 +0300

    added btnClose eventListerner

--- Modal/script.js
+++ Modal/script.js
@@ -16,11 +16,15 @@ const openModal = function () {
   overLay.classList.remove('hidden');
 };
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overLay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overLay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+
+overLay.addEventListener('click', closeModal);
