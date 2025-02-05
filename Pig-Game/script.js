6f6e7f597bef52ccacb44ac9a83a328218f6551e
David Maigwa <davidmaigwa164@gmail.com>
2025-02-05 11:45:03 +0300

    Setting pigGame default setting

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -11,9 +11,11 @@ for (let i = 0; i < scoreDisplay.length; i++) {
 }
 
 dice.style.display = 'none';*/
-
+/*
 //start
 //Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -50,6 +52,23 @@ btnRoll.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
+*/
+
+//************Revision*********** */
+
+//Selecting the elements
+const cumulativeScore0El = document.getElementById('score--0');
+const cumulativeScore1El = document.getElementById('score--1');
+const diceImg = document.querySelector('.dice');
+
+//Default setting
+cumulativeScore0El.textContent = 0;
+cumulativeScore1El.textContent = 0;
+diceImg.classList.add('hidden');
