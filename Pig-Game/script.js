058540a2362dcac0b536d6659c60776a663be048
David Maigwa <davidmaigwa164@gmail.com>
2025-02-05 12:16:19 +0300

    Generating random number for dice on click and adding to current score

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -64,11 +64,32 @@ btnRoll.addEventListener('click', function () {
 //************Revision*********** */
 
 //Selecting the elements
-const cumulativeScore0El = document.getElementById('score--0');
-const cumulativeScore1El = document.getElementById('score--1');
-const diceImg = document.querySelector('.dice');
+const cumulativeScore0El = document.getElementById('score--0'); //cumulative score element player 1
+const cumulativeScore1El = document.getElementById('score--1'); //cumulative score element player 2
+const currentScore0El = document.getElementById('current--0'); //The current score everytime dice is rolled/during active player
+const currentScore1El = document.getElementById('current--1'); //The current score everytime dice is rolled/during active player
+const diceImg = document.querySelector('.dice'); //Dice image element
+const btnRoll = document.querySelector('.btn--roll'); // Selecting the dice roll btn
 
 //Default setting
-cumulativeScore0El.textContent = 0;
-cumulativeScore1El.textContent = 0;
-diceImg.classList.add('hidden');
+cumulativeScore0El.textContent = 0; //setting initial condition for cumulative score player 1
+cumulativeScore1El.textContent = 0; // setting initial condition for cumulative score player 2
+diceImg.classList.add('hidden'); // setting the dice image to hidden (only appears when dice is rolled)
+
+//Declaring variable
+let currentScore = 0;
+
+//Rolling a Dice on click handler
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6 + 1); //Random number generator btwn 1-6
+  // console.log(dice);//confirm if random number is generated
+  diceImg.classList.remove('hidden'); //Removing the hidden class to display the dice now on roll
+  diceImg.src = `dice-${dice}.png`; //dice is dynamically displayed according to the dice number
+
+  //Adding the current score to the existing score everytime a dice is rolled
+  currentScore += dice;
+  // console.log(currentScore);//checking if current score is adding up to the current score
+  // currentScore0El.textContent = currentScore;// setting the content of current cumulative score to player on to check if it is working
+
+  //Cheking if dice ===1 to switch player
+});
