43330e1d42c9c5b95eb26da57ba986e6b46e388d
David Maigwa <davidmaigwa164@gmail.com>
2025-04-29 15:38:21 +0300

    destructing an array before passing it into a function

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -294,3 +294,8 @@ const add = function (...numbers) {
 add(2, 3);
 add(5, 3, 7, 2);
 add(8, 2, 5, 3, 2, 1, 4);
+
+//destructing an array before passing it into a function
+
+const x = [23, 5, 7];
+add(...x);
